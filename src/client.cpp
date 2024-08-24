383266caefb7ca1fda44c0ba1b1819ffa4c5d171
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-08-24 13:49:27 +0900

    [upd] long message

--- src/client.cpp
+++ src/client.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 // 接続の目印となるファイル
 #define SOCKNAME "/tmp/udsock"
@@ -33,9 +36,17 @@ int main(void)
     }
 
     // 送信するデータ
-    char msg[64] = "This is client.";
+    std::stringstream ss;
+    ss << "This is client. Very long message follows:";
+    for (int i = 0; i < 100; i++)
+    {
+        ss << "Hello, World! " << i << std::endl;
+    }
+
     // データの送信
-    send(s, msg, sizeof(msg), 0);
+    const auto num = ss.str().size();
+    const auto sent = send(s, ss.str().c_str(), ss.str().size(), 0);
+    std::cout << "Sent " << sent << " bytes of " << num << " bytes." << std::endl;
 
     // 回線の切断
     close(s);
--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sstream>
+#include <iostream>
 
 // 接続の目印となるファイル
 #define SOCKNAME "/tmp/udsock"
@@ -38,12 +40,18 @@ int main(void)
   int s = accept(sb, NULL, NULL);
 
   // データの受信
-  char msg[64];
-  int len = recv(s, msg, sizeof(msg), 0);
-  msg[len] = '\0';
+  const int buf_size = 64;
+  char buf[buf_size + 1];
+  std::stringstream ss;
+  int read;
+  while ((read = recv(s, buf, buf_size, 0)) > 0)
+  {
+    buf[read] = '\0';
+    ss << buf;
+  }
 
   // データの表示
-  printf("Server received : %s\n", msg);
+  std::cout << "Server received : " << ss.str() << std::endl;
 
   // ソケットの廃止
   close(s);
