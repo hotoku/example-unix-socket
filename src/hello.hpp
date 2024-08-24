8adbc87fd34cf844500dc5737a87572c7042e295
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-08-24 20:18:24 +0900

    [tiny]

--- src/hello.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __HELLO_HPP__
-#define __HELLO_HPP__
-
-void hello();
-
-#endif // __HELLO_HPP__
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "hello.hpp"
-
-int main(){
-    std::cout << "main" << std::endl;
-    hello();
-    return 0;
-}
